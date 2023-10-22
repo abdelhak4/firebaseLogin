d0a63e359f1341eae0c53047e3f33fc0f1f0c7b5
Abdelhak El moussaoui <ael-mous@e1r2p2.1337.ma>
2023-10-22 13:13:27 +0100

    added: profile

--- app/src/main/java/com/example/firebaselogin/MainActivity.kt
+++ app/src/main/java/com/example/firebaselogin/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.firebaselogin.presentation.sign_in.GoogleAuthUiClient
+import com.example.firebaselogin.presentation.sign_in.ProfileScreen
 import com.example.firebaselogin.presentation.sign_in.SignInScreen
 import com.example.firebaselogin.presentation.sign_in.SignInViewModel
 import com.example.firebaselogin.ui.theme.FirebaseLoginTheme
@@ -35,12 +36,13 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    private val  googleAuthUiClient by lazy {
+    private val googleAuthUiClient by lazy {
         GoogleAuthUiClient(
             context = applicationContext,
             oneTapClient = Identity.getSignInClient(applicationContext)
         )
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this);
@@ -54,14 +56,19 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(navController = navController, startDestination = "Sign_in") {
                         composable("Sign_in") {
-                            val viewModel= viewModel<SignInViewModel>()
+                            val viewModel = viewModel<SignInViewModel>()
                             val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            LaunchedEffect(key1 = Unit) {
+                                if (googleAuthUiClient.getSingedInUser() != null) {
+                                    navController.navigate("profile")
+                                }
+                            }
 
                             val launcher = rememberLauncherForActivityResult(
                                 contract = ActivityResultContracts.StartIntentSenderForResult(),
-                                onResult = {result ->
-                                    if (result.resultCode == RESULT_OK)
-                                    {
+                                onResult = { result ->
+                                    if (result.resultCode == RESULT_OK) {
                                         lifecycleScope.launch {
                                             val signInResult = googleAuthUiClient.signInWithIntent(
                                                 intent = result.data ?: return@launch
@@ -73,28 +80,44 @@ class MainActivity : ComponentActivity() {
                             )
 
                             LaunchedEffect(key1 = state.isSignInSuccessful) {
-                                if (state.isSignInSuccessful)
-                                {
+                                if (state.isSignInSuccessful) {
                                     Toast.makeText(
-                                        applicationContext,
-                                        "Sign in s",
+                                        applicationContext, 
+                                        "Sign in done",
                                         Toast.LENGTH_LONG
                                     ).show()
+                                    navController.navigate("profile")
+                                    viewModel.resetState()
                                 }
                             }
 
-                           SignInScreen(state = state, onSignInClick = {
-                               lifecycleScope.launch {
-                                   val signInIntentSender = googleAuthUiClient.signIn()
-                                   launcher.launch(
-                                       IntentSenderRequest.Builder(
-                                           signInIntentSender ?: return@launch
-                                       ).build()
-                                   )
-                               }
-                           })
+                            SignInScreen(state = state, onSignInClick = {
+                                lifecycleScope.launch {
+                                    val signInIntentSender = googleAuthUiClient.signIn()
+                                    launcher.launch(
+                                        IntentSenderRequest.Builder(
+                                            signInIntentSender ?: return@launch
+                                        ).build()
+                                    )
+                                }
+                            })
 
 
+                        }
+                        composable("profile") {
+                            ProfileScreen(
+                                userData = googleAuthUiClient.getSingedInUser()
+                            ) {
+                                lifecycleScope.launch {
+                                    googleAuthUiClient.signOut()
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Signed out",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                    navController.popBackStack()
+                                }
+                            }
                         }
                     }
                 }
---
+++ app/src/main/java/com/example/firebaselogin/presentation/sign_in/ProfileScreen.kt
@@ -0,0 +1,52 @@
+package com.example.firebaselogin.presentation.sign_in
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun ProfileScreen(
+    userData: UserData?,
+    onSignOut: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (userData?.profilePictureUrl != null) {
+            AsyncImage(
+                model = userData.profilePictureUrl,
+                contentDescription = "profile image",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        if (userData?.username != null) {
+            Text(text = userData.username, textAlign = TextAlign.Center)
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        Button(onClick = onSignOut) {
+            Text(text = "Sign Out")
+        }
+    }
+}
